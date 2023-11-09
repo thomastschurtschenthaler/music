a72d67a33b9421ffa5efb860e66108c65d3fd141
Thomas Tschurtschenthaler <thomas.tschurtschenthaler@gmail.com>
2023-11-09 17:52:00 +0100

    test

--- ytviewer/ytviewer.js
+++ ytviewer/ytviewer.js
@@ -306,9 +306,9 @@
                     let audio = ytinfo.formats.filter((f)=>{return (!f.isHLS && f.hasAudio && !f.hasVideo);});
                     let audiovideos = ytinfo.formats.filter((f)=>{return (f.hasAudio && f.hasVideo)});
                     let audiovideo = audiovideos.length>0?audiovideos[0]:null;
-                    let vqalities = [];
+                    let vqualities = [];
                     let video = ytinfo.formats.filter((f)=>{return ((!window.MediaSource || !f.isHLS) && f.hasVideo && (window.MediaSource || f.hasAudio));})
-                        .filter((v)=>{console.log("xxx", v); if (vqalities.indexOf(v.qualityLabel)<0 && (audiovideo==null || v==audiovideo || v.qualityLabel!=audiovideo.qualityLabel)){vqalities.push(v.qualityLabel); return true;}; return false});
+                        .filter((v)=>{console.log("yyy", v, vqualities, v.qualityLabel, vqualities.indexOf(v.qualityLabel)); if (vqualities.indexOf(v.qualityLabel)<0 && (audiovideo==null || v==audiovideo || v.qualityLabel!=audiovideo.qualityLabel)){console.log("aaa", v); vqualities.push(v.qualityLabel); return true;}; return false});
                     videoinfos={"ytinfo":ytinfo, 
                                 "formats":ytinfo.formats.filter((f)=>{return (!f.isHLS || true);}),
                                 "audio":audio.length>0?audio[0]:null,
